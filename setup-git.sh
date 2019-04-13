f5a59abe2dd90e824c7e973304f5653bf7035885
ElectronicManuel <23333606+ElectronicManuel@users.noreply.github.com>
2019-04-13 02:51:11 +0200

    added -y

--- setup-git.sh
+++ setup-git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configure Git credential store
-sudo apt install libsecret-1-0 libsecret-1-dev
+sudo apt -y install libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
 
