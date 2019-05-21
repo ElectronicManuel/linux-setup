c16fca0a36252691dfaf841f471103cd30cb2fa3
Emanuel <23333606+ElectronicManuel@users.noreply.github.com>
2019-05-22 01:01:02 +0200

    Update fix-spotify.sh

--- fix-spotify.sh
+++ fix-spotify.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 SPOTIFY_USERNAME=$1
 
+if [ -z "$SPOTIFY_USERNAME" ];
+then
+     printf "Please enter your spotify username: "
+     read SPOTIFY_USERNAME
+fi
+
+if [ ! -f "~/.config/spotify/Users/$SPOTIFY_USERNAME-user/prefs" ];
+then
+    echo "Could not find a spotify config for your account!"
+    exit 1
+fi
+
 echo "ui.track_notifications_enabled=false" >> ~/.config/spotify/Users/$SPOTIFY_USERNAME-user/prefs
+echo "If the fix did not work, close Spotify and run the script again"
