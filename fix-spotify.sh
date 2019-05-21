a8d8e0071db9c672a09c382ea58567988621d2b2
Emanuel <23333606+ElectronicManuel@users.noreply.github.com>
2019-05-22 00:57:56 +0200

    fix spotify fix

--- fix-spotify.sh
+++ fix-spotify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SPOTIFY_USERNAME=$1
 
-echo "\nui.track_notifications_enabled=false" >> ~/.config/spotify/Users/$SPOTIFY_USERNAME-user/prefs
+echo "ui.track_notifications_enabled=false" >> ~/.config/spotify/Users/$SPOTIFY_USERNAME-user/prefs
